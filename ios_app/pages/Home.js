8d9414b500f6edba74de554113731b8b7cb9d0e7
王聪 <goastwang0131@gmail.com>
2021-04-22 22:26:30 +0800

    首页下拉刷新组件完成

--- ios_app/pages/Home.js
+++ ios_app/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, RefreshControl } from 'react-native';
 import IndexSwiper from '../components/IndexSwiper';
 import MainTitle from '../components/MainTitle';
 import RecomCourseList from '../components/RecomCourseList';
@@ -12,25 +12,57 @@ const HomePage = props => {
   const [fieldData, setFieldData] = useState([]);
   const [courseData, setCourseData] = useState([]);
   const [recomCourseData, setRecomCourseData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const load = () => {
+    courseService
+      .getCourseDatas()
+      .then(res => {
+        setCourseData(res.courses);
+        setFieldData(res.fields);
+        setSwiperData(res.swipers);
+        setRecomCourseData(res.recomCourses);
+      })
+      .finally(() => {
+        setRefreshing(false);
+      });
+  };
+  const onPageRefresh = () => {
+    setRefreshing(true);
+    load();
+  };
   const renderMainTitle = (data, title) => {
     if (data) {
       return data && <MainTitle title={data && data.field_name} />;
-    };
+    }
     return <MainTitle title={title} />;
+  };
+  const renderRefreshControl = options => {
+    const { title, refreshing, onPageRefresh, tintColor, titleColor } = options;
+    return (
+      <RefreshControl
+        title={title}
+        refreshing={refreshing}
+        onRefresh={onPageRefresh}
+        tintColor={tintColor}
+        titleColor={titleColor}
+      />
+    );
   };
   const { navigation } = props;
   useEffect(() => {
-    courseService.getCourseDatas().then(res => {
-      setCourseData(res.courses);
-      setFieldData(res.fields);
-      setSwiperData(res.swipers);
-      setRecomCourseData(res.recomCourses);
-    });
+    load();
   }, []);
   return (
     <ScrollView
       automaticallyAdjustContentInsets={false}
       showsVerticalScrollIndicator={false}
+      refreshControl={renderRefreshControl({
+        titleColor: '#666',
+        tintColor: '#666',
+        title: '正在加载中',
+        refreshing: refreshing,
+        onPageRefresh: onPageRefresh
+      })}
     >
       <IndexSwiper navigation={navigation} swiperData={swiperData} />
       {renderMainTitle(null, '推荐课程')}
